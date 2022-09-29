418e7bda696f10d054bd1afae7c7ad2193cb644a
Sebastian Giraldo <juangiraldoxc@gmail.com>
2022-09-29 18:03:58 -0500

    Intentando conectar AXIOS pero tenemos problemas con los  CORS

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ const axios_api = axios.create({
     baseURl: 'https://api.themoviedb.org/3/',
     headers: {
         'Content-Type': 'application/json;charset=utf8',
+    },
+    params: {
         'api_key': API_KEY,
-    }
+    },
 })
 
 async function getTrendingPreview() {
@@ -26,11 +28,13 @@ async function getTrendingPreview() {
 }
 
 async  function getCategoriesPreview() {
-    // const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
-    const { data } = await axios_api.get('genre/movie/list')
+    const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+
+    // const { data2 } = await axios_api.get('genre/movie/list')
 
     const previewCategoryContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
     
+    const data = await response.json();
     data.genres.forEach( category => {
         const categoryContainer = document.createElement('div')
         categoryContainer.classList.add('category-container')
